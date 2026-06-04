cd81a4d887e4ad9c1e846db4cfe99ed898fe7325
Santiago Escobar <santiago.escobar857@gmail.com>
2026-06-04 10:27:44 -0500

    Quitar archivos innecesarios, se me olvido el gitignore

--- obj/Debug/net10.0/Supletorio Ing Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/Supletorio Ing Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supletorio Ing Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcbc372f6f2ac232f334a6392cf85f760cce9643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5ed71dd4e504a9286f4ebbe4cbf357da21f7bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supletorio Ing Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supletorio Ing Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
